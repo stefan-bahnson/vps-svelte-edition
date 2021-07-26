674d45b92c62289099c6894e75988e2c73bbaa82
Stefan <stefan.bahnson@creatics.se>
2021-07-26 22:07:02 +0200

    change port

--- src/App.svelte
+++ src/App.svelte
@@ -9,7 +9,7 @@
   export let name
 
   const client = new ApolloClient({
-    uri: "https://zdce41a92-zc4c24716-gtw.qovery.io:4000/graphql",
+    uri: "https://zdce41a92-zc4c24716-gtw.qovery.io:8080/graphql",
 
     onError: ({ networkError, graphQLErrors }) => {
       console.log("graphQLErrors", graphQLErrors)
